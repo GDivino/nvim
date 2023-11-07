572fd1b254bd9de3202f8ec9811dfa9b4affeef6
GDevs123 <gio.divino@xendit.co>
2023-11-07 18:40:28 +0800

    chore: show relative path for filename

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -25,7 +25,12 @@ function luaLineConf(content)
     sections = {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename' },
+      lualine_c = {
+        {
+          'filename',
+          path = 1
+        }
+      },
       lualine_x = { 'encoding', 'filetype' },
       lualine_y = { 'progress' },
       lualine_z = { 'location' }
