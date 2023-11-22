f22892f13d906e4c8740ae70cfd416cd81f756a6
GDevs123 <gio.divino@xendit.co>
2023-11-22 08:33:25 +0800

    chore: add spacing for git pop command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -27,6 +27,6 @@ autocmd("BufWinEnter", {
     vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", opts)
     vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", opts)
     vim.keymap.set("n", "<leader>gb", ":Git stash list<CR>", opts)
-    vim.keymap.set("n", "<leader>gB", ":Git stash pop", opts)
+    vim.keymap.set("n", "<leader>gB", ":Git stash pop ", opts)
   end,
 })
