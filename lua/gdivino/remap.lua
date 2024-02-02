427edfc1d2a3d22a74b2b8ffe2298096e9eb6abc
GDivino <giodivino.tech@gmail.com>
2024-02-02 12:54:23 +0800

    chore: remove search shortcut

--- lua/gdivino/remap.lua
+++ lua/gdivino/remap.lua
@@ -31,7 +31,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>r", [[:%s/]])
-vim.keymap.set("n", "<leader>l", "/")
+-- vim.keymap.set("n", "<leader>l", "/")
 vim.keymap.set("v", "<leader>su", ":sort u<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
