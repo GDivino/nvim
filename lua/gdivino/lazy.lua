2e5264c675e5ddd435f0b470f33a6bfdb7679874
GDivino <giodivino.tech@gmail.com>
2024-02-10 08:19:09 +0800

    feat: install noice.nvim for better cmd

--- lua/gdivino/lazy.lua
+++ lua/gdivino/lazy.lua
@@ -90,6 +90,20 @@ local plugins = {
 
     -- ========== mini.nvim ==========
     { 'echasnovski/mini.nvim', version = '*' },
+
+    -- ========== noice.nvim ==========
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    }
 }
 
 require('lazy').setup(plugins, opts)
