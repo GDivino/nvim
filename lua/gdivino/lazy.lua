09ac03208816592c34dccad498604e2d78f9e78a
GDivino <giodivino.tech@gmail.com>
2024-06-05 10:58:29 +0800

    feat: replace mini.nvim with ultimate-autopair plugin

--- lua/gdivino/lazy.lua
+++ lua/gdivino/lazy.lua
@@ -1,11 +1,11 @@
-local lazypath = vim.fn.stdpath('data') .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
         lazypath,
     })
 end
@@ -14,82 +14,86 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
     -- ========== telescope ==========
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.3",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
 
     -- ========== treesitter ==========
     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
     },
-    { 'nvim-treesitter/playground' },
+    { "nvim-treesitter/playground" },
 
     -- ========== fonts ==========
     {
-        'rose-pine/neovim',
-        name = 'rose-pine'
+        "rose-pine/neovim",
+        name = "rose-pine"
     },
 
     -- ========== harpoon ==========
-    { 'theprimeagen/harpoon' },
+    { "theprimeagen/harpoon" },
 
     -- ========== undotree ==========
-    { 'mbbill/undotree' },
+    { "mbbill/undotree" },
 
     -- ========== fugitive ==========
-    { 'tpope/vim-fugitive' },
+    { "tpope/vim-fugitive" },
 
     -- ========== LSP ==========
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x'
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x"
     },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
 
     -- ========== nerdcommenter ==========
-    { 'preservim/nerdcommenter' },
+    { "preservim/nerdcommenter" },
 
     -- ========== yaml neovim ==========
     {
-        'cuducos/yaml.nvim',
-        ft = { 'yaml' }, -- optional
+        "cuducos/yaml.nvim",
+        ft = { "yaml" }, -- optional
         dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-telescope/telescope.nvim', -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim", -- optional
         },
     },
 
     -- ========== lualine ==========
     {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         dependencies = {
-            'nvim-tree/nvim-web-devicons', opt = true,
+            "nvim-tree/nvim-web-devicons", opt = true,
         }
     },
 
     -- ========== copilot ==========
-    -- { 'github/copilot.vim' },
+    -- { "github/copilot.vim" },
 
     -- ========== octo ==========
     {
-        'pwntester/octo.nvim',
+        "pwntester/octo.nvim",
         dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-            -- OR 'ibhagwan/fzf-lua',
-            'nvim-tree/nvim-web-devicons',
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            -- OR "ibhagwan/fzf-lua",
+            "nvim-tree/nvim-web-devicons",
         }
     },
 
-    -- ========== mini.nvim ==========
-    { 'echasnovski/mini.nvim', version = '*' },
+    -- ========== ultimate auto pair.nvim ==========
+    {
+        "altermo/ultimate-autopair.nvim",
+        event = { "InsertEnter", "CmdlineEnter" },
+        branch = "v0.6", --recommended as each new version will have breaking changes
+    },
 
     -- ========== noice.nvim ==========
     {
@@ -106,4 +110,4 @@ local plugins = {
     }
 }
 
-require('lazy').setup(plugins, opts)
+require("lazy").setup(plugins, opts)
