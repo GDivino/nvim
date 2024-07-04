314328b81179c54132c72207fa7133cfa5f7436d
IngEurCDaaS01 <giancarlo.lorenzo.divino@ing.com>
2024-07-04 11:22:06 +0800

    fix: add BufNew autocmd event to lualine YAMl

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -41,7 +41,7 @@ function luaLineConf(content)
     })
 end
 
-autocmd({ 'BufWinEnter', 'BufEnter', 'FileType' }, {
+autocmd({ 'BufWinEnter', 'BufEnter', 'BufNew', 'FileType' }, {
     group = GDivino_Lualine,
     pattern = '*',
     callback = function()
