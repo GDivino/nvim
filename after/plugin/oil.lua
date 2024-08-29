b4f8b1d7c8f15e4014ace62175975c58827755e7
IngEurCDaaS01 <giancarlo.lorenzo.divino@ing.com>
2024-08-29 20:01:45 +0800

    fix: oil configuration

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,1 +1,9 @@
-require("oil").setup()
+require("oil").setup({
+    keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+    },
+})
--- lua/gdivino/lazy.lua
+++ lua/gdivino/lazy.lua
@@ -121,7 +121,7 @@ local plugins = {
         'stevearc/oil.nvim',
         opts = {},
         -- Optional dependencies
-        dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
         -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     },
 }
