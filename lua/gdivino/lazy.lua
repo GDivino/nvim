dc8714dccd25ad4c0d068730f8fe6d59ea15927f
GDivino <giodivino.tech@gmail.com>
2024-09-16 22:46:24 +0800

    fix: clean up plugins

--- lua/gdivino/lazy.lua
+++ lua/gdivino/lazy.lua
@@ -81,20 +81,6 @@ local plugins = {
         branch = "v0.6", --recommended as each new version will have breaking changes
     },
 
-    -- ========== noice.nvim ==========
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        }
-    },
-
     -- ========== indent-blankline.nvim ==========
     {
         "lukas-reineke/indent-blankline.nvim",
