fce714bba2c7c681dd3701b9b7c0bb01a9285b36
GDivino <giodivino.tech@gmail.com>
2024-11-04 16:11:24 +0800

    fix: set up preview shortcut in oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,4 +1,7 @@
+vim.opt.splitright = true
+
 require("oil").setup({
+    default_file_explorer = false,
     keymaps = {
         ["<CR>"] = "actions.select",
         ["<C-h>"] = false,
@@ -6,8 +9,10 @@ require("oil").setup({
         ["-"] = "actions.parent",
         ["_"] = "actions.open_cwd",
         ["<C-r>"] = "actions.refresh",
+        ["<C-p>"] = "actions.preview"
     },
     view_options = {
         show_hidden = true,
     }
 })
+
