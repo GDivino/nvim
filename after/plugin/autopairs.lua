9e45098c1d8ab921d46aa39f16ff60571aaddea1
Gian Divino <giancarlo.lorenzo.divino@ing.com>
2025-04-28 13:26:08 +0800

    feat: add "`" in fastwrap

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -1,6 +1,16 @@
 local autopairs = require("nvim-autopairs")
+
 autopairs.setup({
     fast_wrap = {
+        chars = {
+            "{",
+            "[",
+            "***",
+            "(",
+            '"',
+            "'",
+            "`"
+        },
         map = "<A-w>",
         end_key = "l",
         after_key = "j"
