9d2f1d5aa8043abb9e691a6951da54c00626089d
Gian Divino <giancarlo.lorenzo.divino@ing.com>
2026-01-09 10:50:48 +0800

    feat: set up word wrap for nvim

--- lua/gdivino/set.lua
+++ lua/gdivino/set.lua
@@ -12,9 +12,39 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 vim.opt.linebreak = false
+vim.opt.breakindent = false
+vim.opt.showbreak = ""
 vim.keymap.set("n", "<leader>w", function()
     vim.opt.wrap = not vim.opt.wrap:get()
     vim.opt.linebreak = not vim.opt.linebreak:get()
+    vim.opt.breakindent = not vim.opt.breakindent:get()
+    vim.opt.relativenumber = not vim.opt.relativenumber:get()
+
+    local wrap_enabled = vim.opt.wrap:get()
+
+    if wrap_enabled then
+        vim.keymap.set('n', 'j', 'gj')
+        vim.keymap.set('n', 'k', 'gk')
+        vim.keymap.set('n', '<Down>', 'gj')
+        vim.keymap.set('n', '<Up>', 'gk')
+        vim.keymap.set('n', '0', 'g0')
+        vim.keymap.set('n', '$', 'g$')
+        vim.keymap.set('n', '_', 'g^')
+        vim.keymap.set('n', '<S-a>', function()
+            vim.api.nvim_feedkeys('g$a', 'n', false)
+        end)
+        vim.opt.showbreak = "↳ "
+    else
+        vim.keymap.set('n', 'j', 'j')
+        vim.keymap.set('n', 'k', 'k')
+        vim.keymap.set('n', '<Down>', '<Down>')
+        vim.keymap.set('n', '<Up>', '<Up>')
+        vim.keymap.set('n', '0', '0')
+        vim.keymap.set('n', '$', '$')
+        vim.keymap.set('n', '_', '_')
+        vim.keymap.set('n', '<S-a>', 'A')
+        vim.opt.showbreak = ""
+    end
 end)
 
 vim.opt.swapfile = false
